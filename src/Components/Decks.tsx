2223cb3e2d75cd36f4b59bd123e29ea0289bf2a0
Jhorner22 <jphorner22@gamil.com>
2023-06-27 00:58:31 -0500

    fix

--- src/Components/Decks.tsx
+++ src/Components/Decks.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
+// import archidekt from 'archidekt';
 
 function Decks() {
+// console.log(
+//     archidekt.searchDecks(
+//         {
+//             owner:"HyperWalrus"
+//         }
+//     )
+// )
+
 return (
     <div className='flex flex-col gap-3 m-2 text-[#2A2539]'>
       <Navbar/>
         <div className='flex lg:flex-row flex-col h-full  gap-2'>
             <div className='flex rounded-[30px] bg-[#EAE9E5] p-6 flex flex-col justify-between'>
-            Coming Soon...
+
             </div>
         </div>
     </div>
