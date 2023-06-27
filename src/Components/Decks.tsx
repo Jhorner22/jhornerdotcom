fe1d511dad989647ecb2d8d7fb4433d6614c46ef
Jhorner22 <jphorner22@gamil.com>
2023-06-27 01:47:24 -0500

    DECKS

--- src/Components/Decks.tsx
+++ src/Components/Decks.tsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Navbar from "./Navbar";
-// import archidekt from 'archidekt';
+import { Deck, RawDeck } from "../Types/Decks";
+
 
 function Decks() {
-// console.log(
-//     archidekt.searchDecks(
-//         {
-//             owner:"HyperWalrus"
-//         }
-//     )
-// )
+
+    const [decks,setDecks] = useState([] as Deck[])
+
+    
+    useEffect(() => {
+        async function getDecks() {
+             const decks = await fetch("https://archidekt.com/api/decks/cards/?owner=HyperWalrus")
+            .then((response) => response.json() as unknown as RawDeck)
+            setDecks(decks.results)
+        }
+        getDecks()
+    },[])
 
 return (
     <div className='flex flex-col gap-3 m-2 text-[#2A2539]'>
       <Navbar/>
         <div className='flex lg:flex-row flex-col h-full  gap-2'>
             <div className='flex rounded-[30px] bg-[#EAE9E5] p-6 flex flex-col justify-between'>
-
+                {decks.map((deck) => {
+                    return(
+                        <p>
+                            {
+                                deck.name
+                            }
+                        </p>
+                    )
+                })}
             </div>
         </div>
     </div>
---
+++ src/Types/Decks.ts
@@ -0,0 +1,10 @@
+export interface RawDeck{
+    count:number
+    next:string
+    previous:string
+    results:Deck[]
+}
+
+export interface Deck{
+    name:string;
+}
